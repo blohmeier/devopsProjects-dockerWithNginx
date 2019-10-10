ce54abdca95443feb3b3deb342b59b5d7c00052c
blohmeier <brett.lohmeier@gmail.com>
2019-10-10 01:45:58 -0500

    updated test.py with first additional lint test

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
+
+import time
+from selenium import webdriver
 import unittest
 import app
 
-import unittest
-import app
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title) 
+        self.fail('Finish the test!')
 
 class TestHello(unittest.TestCase):
 
@@ -12,9 +25,9 @@ class TestHello(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_hello(self):
-        rv = self.app.get('/')
+        rv = self.app.get('/works/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'works\n')
 
     def test_hello_hello(self):
         rv = self.app.get('/hello/')
@@ -22,10 +35,10 @@ class TestHello(unittest.TestCase):
         self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_name(self):
-        name = 'Simon'
+        name = 'Brett'
         rv = self.app.get(f'/hello/{name}')
         self.assertEqual(rv.status, '200 OK')
         self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(warnings='ignore')
